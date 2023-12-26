b3d13d37474ba021f2ca12e71e3963f93e006242
Michal Koziel <michal.koziel@remarkable.no>
2023-12-26 21:45:03 +0100

    Ender-3 S1 Plus Pro: port to PROBE_OFFSET_Z_MIN/MAX
    Signed-off-by: Michal Koziel <michal.koziel@remarkable.no>

--- config/examples/Creality/Ender-3 S1 Plus Pro/Configuration.h
+++ config/examples/Creality/Ender-3 S1 Plus Pro/Configuration.h
@@ -1646,8 +1646,8 @@
 #define Z_PROBE_LOW_POINT          -2 // Farthest distance below the trigger-point to go before stopping
 
 // For M851 give a range for adjusting the Z probe offset
-#define Z_PROBE_OFFSET_RANGE_MIN -5
-#define Z_PROBE_OFFSET_RANGE_MAX 10
+#define PROBE_OFFSET_Z_MIN -5
+#define PROBE_OFFSET_Z_MAX 10
 
 // Enable the M48 repeatability test to test probe accuracy
 #define Z_MIN_PROBE_REPEATABILITY_TEST
